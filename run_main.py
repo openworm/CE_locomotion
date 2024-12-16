1e6b15d07de6cdf4a121c64eb662681968058a05
adampdp <adampdp100@gmail.com>
2024-12-16 21:38:37 +0000

    updated arg process

--- run_main.py
+++ run_main.py
@@ -3,6 +3,86 @@ import argparse
 import os
 import sys
 import helper_funcs as hf
+from pyneuroml.utils.cli import build_namespace
+
+DEFAULTS = {
+    "popSize": 96,
+    "duration": 24,
+    "simsep": False,
+    "RandSeed": 42,
+    "randSeed": 42,
+    "folderName": None,
+    "doEvol": False
+    }
+
+def process_args():
+    """Parse command-line arguments.
+
+    :returns: None
+    """
+    parser = argparse.ArgumentParser(
+        description=(
+            "A script which can be run to execute Worm2D"
+        )
+    )
+
+    parser.add_argument(
+        "--simsep", action="store_true", default=DEFAULTS["simsep"],
+        help=("If used, interactive mode is requested."),
+    )
+    
+    parser.add_argument(
+        "--doEvol", action="store_true", default=DEFAULTS["doEvol"],
+        help=("If used evolutionary algorithm is executed."),
+    )
+
+    parser.add_argument(
+        "--folderName",
+        type=str,
+        metavar="<folder name>",
+        default=DEFAULTS["folderName"],
+        help="Name of folder to store or process output.",
+    )    
+
+    parser.add_argument(
+        "-d",
+        "--duration",
+        type=float,
+        metavar="<duration>",
+        default=DEFAULTS["duration"],
+        help="Duration of simulation in ms, default: %sms" % DEFAULTS["duration"],
+    )
+
+    parser.add_argument(
+        "-p",
+        "--popSize",
+        type=int,
+        metavar="<pop size>",
+        default=DEFAULTS["popSize"],
+        help="Population size for evolutionary algorithm, default: %s" % DEFAULTS["popSize"],
+    )
+
+    parser.add_argument(
+        "-R",
+        "--RandSeed",
+        type=int,
+        metavar="<Rand seed>",
+        default=DEFAULTS["RandSeed"],
+        help="Absolute seed value, default: %s" % DEFAULTS["RandSeed"],
+    )
+    
+    parser.add_argument(
+        "-r",
+        "--randSeed",
+        type=int,
+        metavar="<rand seed>",
+        default=DEFAULTS["randSeed"],
+        help="Relative seed value, default: %s" % DEFAULTS["randSeed"],
+    )
+ 
+    return parser.parse_args()
+
+
 
 def make_directory(directory_name):
     try:
@@ -19,26 +99,35 @@ def make_directory(directory_name):
         print(f"An error occurred: {e}")
         sys.exit(1)
 
+def run_main(args=None):
+    if args is None:
+        args = process_args()
+    run(a=args)
 
-def run_cpp_program(): 
+def run(a = None, **kwargs):
+    #a = build_namespace(DEFAULTS, a, **kwargs)
     
-    parser=argparse.ArgumentParser(description="argument parser")
+
+    
+    """     parser=argparse.ArgumentParser(description="argument parser")
 
     parser.add_argument("-R", "--Rand_seed", type=int, nargs='?', default=42)
     parser.add_argument("-r", "--rand_seed", type=int, nargs='?', default=42)
     parser.add_argument('-p', "--pop_size", type=int, nargs='?', default=96)
     parser.add_argument('-d', "--duration", type=int, nargs='?', default=24)
     parser.add_argument('--simsep', action='store_true')
+    parser.add_argument('-E', '--evolve_folder', type=str, nargs='?', default=None)
+    parser.add_argument('-S', '--sim_folder', type=str, nargs='?', default=None)
 
-    args=parser.parse_args()
+    args=parser.parse_args() """
+
     folder_name = ''
     do_evol = 1
     
-    if args.simsep:
+    if a.simsep:
         while True:
             do_evol_str = input("Do you want to perform an evolutionary search (E) or run a simulation (S)? ")
-            if do_evol_str == "E":
-                do_evol = 1
+            if do_evol_str == "E":                    
                 while True:
                     folder_name = input("Please enter the name of a folder to store data: ")
                     if make_directory(folder_name): break
@@ -51,17 +140,25 @@ def run_cpp_program():
                     print("Folder does not exist.")
                 print('Running simulation.')        
                 break
+    elif a.folderName:
+        folder_name = a.folderName
+        if a.doEvol:
+            if not make_directory(folder_name) : sys.exit(1)
+        else:
+            do_evol = 0
+            if not os.path.isdir(folder_name):
+               print(f"Directory '{folder_name}' does not exist.")
+               sys.exit(1)
     else:
-        print('Running without "--simsep" argument for directory creation.')   
-      
-    
-    if args.Rand_seed is not None:
-        cmd = ['./main', '-R', str(args.Rand_seed)]
+        print('Running in default mode.')
+
+    if a.RandSeed is not None:
+        cmd = ['./main', '-R', str(a.RandSeed)]
     else:    
-        cmd = ['./main', '-r', str(args.rand_seed)]
+        cmd = ['./main', '-r', str(a.randSeed)]
         
-    cmd += ['-p', str(args.pop_size), 
-            '-d', str(args.duration),
+    cmd += ['-p', str(a.popSize), 
+            '-d', str(a.duration),
             '--doevol', str(do_evol),
             '--folder', folder_name]
 
@@ -80,10 +177,11 @@ def run_cpp_program():
     if result.stderr: 
         print("Error:") 
         print(result.stderr)
-
-    if args.simsep:
+    
+    if folder_name!='':
+    #if args.simsep or args.evolve_folder or args.sim_folder:
        hf.dir_name = folder_name
        import load_data    
     
 if __name__ == "__main__": 
-    run_cpp_program() 
+    run_main() 
