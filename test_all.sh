6e07d3182fe640b364ae0f483453614daba624da
Padraig Gleeson <p.gleeson@gmail.com>
2024-12-20 14:50:25 +0000

    Changing seed & retested example on Ubuntu

--- test_all.sh
+++ test_all.sh
@@ -5,5 +5,5 @@ make clean
 make
 
 rm -rf exampleRun
-time python run_main.py -R 1234 -p 96 --doEvol --folderName exampleRun
+time python run_main.py -R 1233 -p 96 --doEvol --folderName exampleRun
 
