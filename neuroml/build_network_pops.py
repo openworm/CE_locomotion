75a9c4efe5cf9144c4e9b3ad1a7971cc53acf7c7
adampdp <adampdp100@gmail.com>
2025-01-20 17:37:16 +0000

    update with population relative cell names

--- neuroml/build_network_pops.py
+++ neuroml/build_network_pops.py
@@ -45,8 +45,15 @@ def get_cell_id_string(pop_id, cell_id, cell_number):
         return "../%s[%s]" % (pop_id, str(cell_number))
 
 
-        
-
+def getPopRelativeCellIndices(pop_names, cell_names):
+    rel_names = list(range(len(cell_names)))
+    for pop_name in pop_names:
+        indices = [i for i, val in enumerate(cell_names) if val == pop_name]
+        for i, val in enumerate(indices):
+            rel_names[val] = i
+    return rel_names         
+     
+    
 with open('../exampleRun/worm_data.json', 'r') as file:
          network_json_data = json.load(file)
 
@@ -58,6 +65,10 @@ num_unit = network_json_data["Worm global parameters"]["N_units"]["value"]
 cell_names = network_json_data["Nervous system"]["Cell name"]["value"]
 pop_names = cell_names[:cell_per_unit]
 
+# generate population relative cell indices
+
+do_rel_indices = True
+rel_indices = getPopRelativeCellIndices(pop_names, cell_names)
 
 nml_doc = NeuroMLDocument(id="Worm2D")
 nml_doc.includes.append(IncludeType(href='cell_syn_X.xml'))
@@ -66,14 +77,27 @@ nml_doc.includes.append(IncludeType(href='cell_syn_X.xml'))
 syn0 = ExpOneSynapse(id="syn0", gbase="65nS", erev="0mV", tau_decay="3ms")
 nml_doc.exp_one_synapses.append(syn0)
 
-
 net = Network(id="Worm2DNet")
 nml_doc.networks.append(net)
 
 #make populations, one for each cell type
 
-
 cell_comp = "GenericNeuronCellX"
+
+if False:
+    IafCell0 = IafCell(
+    id="iaf0",
+    C="1.0 nF",
+    thresh="-50mV",
+    reset="-65mV",
+    leak_conductance="10 nS",
+    leak_reversal="-65mV",)
+
+    nml_doc.iaf_cells.append(IafCell0)
+    cell_comp = IafCell0.id
+
+
+
 #pop0 = Population(id="AllCells", component=cell_comp, size=size0)
 
 for pop_name in pop_names:
@@ -111,16 +135,6 @@ for connection in electrical_weights:
         net.electrical_projections.append(proj0)
 
 
-
-""" elProj0 = ElectricalProjection(
-                    id="ElectricalProj",
-                    presynaptic_population=pop0.id,
-                    postsynaptic_population=pop0.id,
-                )
-
-net.electrical_projections.append(elProj0)
- """
-
 add_PG = False
 if add_PG:
     for pre in range(0, size0):
@@ -143,9 +157,14 @@ if make_connections:
     for index, connection in enumerate(chemical_weights): 
         pre_index = connection["from"] - 1 # zero indexing
         post_index = connection["to"] - 1 # zero indexing
-        weight = connection["weight"]
         pre_pop = cell_names[pre_index]
         post_pop = cell_names[post_index]
+        weight = connection["weight"]
+        if do_rel_indices:
+            pre_index = rel_indices[pre_index]
+            post_index = rel_indices[post_index]
+        
+        
         pre_cell_id = get_cell_id_string(pre_pop, cell_comp, pre_index)
         post_cell_id = get_cell_id_string(post_pop, cell_comp, post_index)
 
@@ -161,34 +180,20 @@ if make_connections:
         net.continuous_projections[chemProjNames.index(chemProjName)].continuous_connection_instance_ws.append(conn0)
 
 
-""" if make_connections:
-    for index, connection in enumerate(chemical_weights): 
-        pre_index = connection["from"] - 1 # zero indexing
-        post_index = connection["to"] - 1 # zero indexing
-        weight = connection["weight"]
-
-        pre_cell_id = get_cell_id_string(pop0.id, IafCell0.id, pre_index)
-        post_cell_id = get_cell_id_string(pop0.id, IafCell0.id, post_index)
-
-        conn0 = ConnectionWD(
-                    id=str(index),
-                    pre_cell_id=pre_cell_id,
-                    post_cell_id=post_cell_id,
-                    weight=weight,
-                    delay="0ms",
-                )
-
-        proj0.connection_wds.append(conn0) """
-
 synclass='gapJunction0'
 make_electric_connections = True
 if make_electric_connections:
     for index, connection in enumerate(electrical_weights): 
         pre_index = connection["from"] - 1 # zero indexing
         post_index = connection["to"] - 1 # zero indexing
-        weight = connection["weight"]
         pre_pop = cell_names[pre_index]
         post_pop = cell_names[post_index]
+        weight = connection["weight"]
+        if do_rel_indices:
+            pre_index = rel_indices[pre_index]
+            post_index = rel_indices[post_index]
+        
+        
         pre_cell_id = get_cell_id_string(pre_pop, cell_comp, pre_index)
         post_cell_id = get_cell_id_string(post_pop, cell_comp, post_index)
 
@@ -203,30 +208,6 @@ if make_electric_connections:
         elecProjName = get_projection_id(pre_pop, post_pop, synclass)
         #proj0.continuous_connection_instance_ws.append(conn0)
         net.electrical_projections[elecProjNames.index(elecProjName)].electrical_connection_instance_ws.append(conn0)
-
-
-""" make_electric_connections = True
-if make_electric_connections:
-    for index, connection in enumerate(electrical_weights): 
-        pre_index = connection["from"] - 1 # zero indexing
-        post_index = connection["to"] - 1 # zero indexing
-        weight = connection["weight"]
-
-        pre_cell_id = get_cell_id_string(pop0.id, IafCell0.id, pre_index)
-        post_cell_id = get_cell_id_string(pop0.id, IafCell0.id, post_index)
-
-        
-        conn0 = ElectricalConnectionInstanceW(
-                    id=str(index),
-                    pre_cell=pre_cell_id,
-                    post_cell=post_cell_id,
-                    #synapse=syn_new.id,
-                    weight=weight,
-                )
-
-        elProj0.electrical_connection_instance_ws.append(conn0) """
-    
-
 
 
 
@@ -250,6 +231,7 @@ print("Written network file to: " + nml_file)
 ###### Validate the NeuroML ######
 
 from neuroml.utils import validate_neuroml2
+#validate_neuroml2(nml_file)
 
 try:
     validate_neuroml2(nml_file)
