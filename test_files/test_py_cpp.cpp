d078d1f13013f9a95567124cc5378e607fa8bfab
pgleeson <p.gleeson@gmail.com>
2025-01-22 09:16:23 +0000

    Initial C++ Python tests

---
+++ test_files/test_py_cpp.cpp
@@ -0,0 +1,29 @@
+// =============================================================
+// Tests for C++ code
+// =============================================================
+
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <stdio.h>
+#include <Python.h>
+
+void test()
+{
+    std::cout << "Created aa test..." << std::endl;
+
+    //Py_Initialize();
+}
+int main(int argc, const char *argv[])
+{
+
+  //Py_Initialize();
+
+    std::cout << "Running a number of tests..." << std::endl;
+
+    test();
+
+    std::cout << "Done!" << std::endl;
+
+    return 0;
+}
