1bd08644bc306e503716555d47dd88938b5910a8
adampdp <adampdp100@gmail.com>
2025-02-07 17:18:04 +0000

    add NervousSystemInt

---
+++ NervousSystemInt.cpp
@@ -0,0 +1,9 @@
+#include "NervousSystemInt.h"
+
+template<class T> 
+void NervousSystemInt<T>::EulerStep(double stepsize)
+{
+    T& underlying = static_cast<T&>(*this);
+    underlying.EulerStep(stepsize);
+}
+
