4183ae613f8d5ba4d53610c66d7aa26fac11b9d7
adampdp <adampdp100@gmail.com>
2025-02-07 16:52:01 +0000

    fix macos issue

--- NervousSystemBase.cpp
+++
@@ -1,9 +0,0 @@
-#include "NervousSystemBase.h"
-
-template<class T> 
-void NervousSystemInt<T>::EulerStep(double stepsize)
-{
-    T& underlying = static_cast<T&>(*this);
-    underlying.EulerStep(stepsize);
-}
-
--- NervousSystemBase.h
+++ NervousSystemBase.h
@@ -1,46 +1,4 @@
 #pragma once
-#include <functional>
-
-using std::function;
-
-
-
-
-template<class T>
-class NervousSystemInt
-{
-    public:
-    NervousSystemInt():derived(static_cast<T&>(*this)){}
-
-    void EulerStep(double stepsize)
-    {
-    //T& underlying = static_cast<T&>(*this);
-    derived.EulerStep(stepsize);
-    //func(stepsize, EulerStep);
-    }
-    
-    double NeuronOutput(int i)
-    {
-    //T& underlying = static_cast<T&>(*this);
-    derived.NeuronOutput(i);
-    }
-    
-    private:
-   
-    T& derived; // = static_cast<T&>(*this);
-
-    void func(double a, function<void(double)> calc) {
-    T& underlying = static_cast<T&>(*this);
-    underlying.calc(a);
-    }
-
-    double func(double a, function<double(int,int)> calc) {
-    T& underlying = static_cast<T&>(*this);
-    underlying.calc(a);
-    }
-   
-
-};
 
 class NervousSystemBase {
     public:
