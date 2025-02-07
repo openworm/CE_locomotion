726db64e4c9e0ced8f12b01775a471258d6015ef
adampdp <adampdp100@gmail.com>
2025-02-07 13:42:21 +0000

    temporary stash

---
+++ Worm.cpp
@@ -0,0 +1,16 @@
+//
+//  Worm.cpp
+//  one
+//
+//  Created by Eduardo Izquierdo on 9/25/15.
+//  Copyright © 2015 Eduardo Izquierdo. All rights reserved.
+//
+
+#include "Worm.h"
+
+int nn(int neuronNumber, int unitNumber)
+{
+  return neuronNumber+((unitNumber-1)*N_neuronsperunit);
+}
+
+
