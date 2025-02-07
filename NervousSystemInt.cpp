5b873a12a68b92ede8036a54b6b67e01e903ef97
adampdp <adampdp100@gmail.com>
2025-02-07 22:06:00 +0000

    remove file

--- NervousSystemInt.cpp
+++
@@ -1,9 +0,0 @@
-#include "NervousSystemInt.h"
-
-template<class T> 
-void NervousSystemInt<T>::EulerStep(double stepsize)
-{
-    T& underlying = static_cast<T&>(*this);
-    underlying.EulerStep(stepsize);
-}
-
