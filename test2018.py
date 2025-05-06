3e6bed1162fa9c41c276ef5930677fe174a043ac
adampdp <adampdp100@gmail.com>
2025-05-06 12:44:26 +0100

    changed 2018 tests

--- test2018.py
+++ test2018.py
@@ -1,9 +1,9 @@
 from run_main import run
 
 run(
-    maxGens=30,
+    maxGens=36,
     popSize=36,
-    RandSeed=323404,
+    RandSeed=292,
     modelFolder="RoyalSociety2018",
     outputFolderName="exampleRunRS18",
     doEvol=True,
--- test2018W2D.py
+++ test2018W2D.py
@@ -1,9 +1,9 @@
 from run_main import run
 
 run(
-    maxGens=30,
+    maxGens=36,
     popSize=36,
-    RandSeed=323404,
+    RandSeed=292,
     modelName="RS18",
     modelFolder="Worm2D",
     outputFolderName="exampleRunRS18W2D",
