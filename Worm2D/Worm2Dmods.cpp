fc4b0868bcf029d47c7c6bd39c1738b3d862632c
adampdp <adampdp100@gmail.com>
2025-08-07 20:26:47 +0900

    added NSosc Nervous system

--- Worm2D/Worm2Dmods.cpp
+++ Worm2D/Worm2Dmods.cpp
@@ -1,5 +1,76 @@
 #include "Worm2Dmods.h"
 
-Worm2Dosc::Worm2Dosc():Worm2D({7,24,0.1,7,49},0),
-Worm2Dm({7,24,0.1,7,49}, 0, new Muscles){}
+Worm2Dosc::Worm2Dosc(const pfa & pfa_):Worm2D({pfa_.size,24,0.1,1,pfa_.size},0),
+Worm2Dm({pfa_.size,24,0.1,1,pfa_.size}, new NSosc(pfa_), new Muscles),n(dynamic_cast<NSosc&>(*n_ptr))
+{
+    setUpMuscleConn();
+    n.setTime(t);
+}
 
+Worm2Dosc::Worm2Dosc(TVector<double> &v):Worm2Dosc(getPfaFromPheno(v)){}
+
+void Worm2Dosc::InitializeState(RandomState &rs)
+{    
+    cout << "Worm2Dosc init state" << endl;
+    Worm2D::InitializeState(rs);
+    return;    
+}
+
+void Worm2Dosc::Step1()
+{
+
+    n.setTime(t);
+    // Update Body
+    b.StepBody(settedStepSize);
+    
+    // Update Nervous System
+    //n_ptr->EulerStep(settedStepSize);
+    
+    // Interneuron input  //////////////////////
+    /* for (int i = 1; i <= par1.N_units; i++){
+        n_ptr->SetNeuronExternalInput(nn(DB, i), wAVB_DB * AVB);
+        n_ptr->SetNeuronExternalInput(nn(VB, i), wAVB_VB * AVB);
+        n_ptr->SetNeuronExternalInput(nn(DA, i), wAVA_DA * AVA);
+        n_ptr->SetNeuronExternalInput(nn(VA, i), wAVA_VA * AVA);
+    
+    } */
+    
+    setMuscleInput();
+    //setMuscleInputOrig(StepSize);   
+
+
+    // Set input to Body
+    //  First two segments receive special treatment because they are only affected by a single muscle
+    b.SetDorsalSegmentActivation(1, m_ptr->DorsalMuscleOutput(1)/2);
+    b.SetVentralSegmentActivation(1, m_ptr->VentralMuscleOutput(1)/2);
+    b.SetDorsalSegmentActivation(2, m_ptr->DorsalMuscleOutput(1)/2);
+    b.SetVentralSegmentActivation(2, m_ptr->VentralMuscleOutput(1)/2);
+    
+    //  All other segments receive force from two muscles
+    for (int i = 3; i <= N_segments-2; i++)
+    {
+        int mi = (int) ((i-1)/2);
+        b.SetDorsalSegmentActivation(i, (m_ptr->DorsalMuscleOutput(mi) + m_ptr->DorsalMuscleOutput(mi+1))/2);
+        b.SetVentralSegmentActivation(i, (m_ptr->VentralMuscleOutput(mi) + m_ptr->VentralMuscleOutput(mi+1))/2);
+    }
+    
+    //  Last two segments receive special treatment because they are only affected by a single muscle
+    b.SetDorsalSegmentActivation(N_segments-1, m_ptr->DorsalMuscleOutput(par1.N_muscles)/2);
+    b.SetVentralSegmentActivation(N_segments-1, m_ptr->VentralMuscleOutput(par1.N_muscles)/2);
+    b.SetDorsalSegmentActivation(N_segments, m_ptr->DorsalMuscleOutput(par1.N_muscles)/2);
+    b.SetVentralSegmentActivation(N_segments, m_ptr->VentralMuscleOutput(par1.N_muscles)/2);
+    
+   
+}
+
+NSosc::NSosc(const pfa & pfa_)
+{
+pfa1.size = pfa_.size;
+for (int i=0;i<pfa_.size;i++)
+{
+pfa1.phase.push_back(pfa_.phase[i]);
+pfa1.freq.push_back(pfa_.freq[i]);
+pfa1.amp.push_back(pfa_.amp[i]);
+}
+
+}
--- Worm2D/Worm2Dmods.h
+++ Worm2D/Worm2Dmods.h
@@ -5,22 +5,51 @@ class Worm2Dosc : public Worm2D
 {
 
 public:
-Worm2Dosc();
+Worm2Dosc(TVector<double> &v);
+Worm2Dosc::Worm2Dosc(const pfa & pfa_);
 const string getModelName() {return "Worm2Dosc";}
 const vector<string> getCellNames() {return;}
-void InitializeState(RandomState &rs) {return;}
+void InitializeState(RandomState &rs);
 void initForSimulation(RandomState &) {return;}
 void DumpParams(ofstream &ofs) {return;}
+pfa getPfaFromPheno(TVector<double> &v){}
 
 protected:
 vector<doubIntParamsHead> getWormParams() {return;}
-void Step1() {return;}
+void Step1();
 const vector<string> getVMuscNames() {return;}
 const vector<string> getDMuscNames() {return;}
 
 vector<toFromWeight> makeVentralMuscleConn() {return;}
 vector<toFromWeight> makeDorsalMuscleConn() {return;}
 
+
+NSosc & n;
+
 };
 
-Worm2Dosc w;
+struct pfa{
+vector<double> phase, freq, amp;
+int size;
+};
+
+class NSosc : public NSForW2D {
+
+public:
+
+NSosc(const pfa & pfa_);
+
+double NeuronOutput(int i) {return pfa1.amp[i]*sin(pi2*pfa1.freq[i]*t + pfa1.phase[i]);}
+double NeuronState(int i) {return 0;}
+void SetNeuronExternalInput(int i, double value) {return;}
+void EulerStep(double stepsize) {return;}
+virtual ~NSosc(){};
+void setTime(const double & t_){t=t_;}
+
+private:
+double t;
+pfa pfa1;
+//vector<double> phase, freq, amp;
+const double pi2 = 3.14159265*2.0;
+
+};
