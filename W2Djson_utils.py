fe9e0c5ab77c35c84e002966b756b5064c604831
adampdp <adampdp100@gmail.com>
2026-01-10 13:26:06 +0000

    json utilities

---
+++ W2Djson_utils.py
@@ -0,0 +1,12 @@
+def joinJson(json1, json2):
+    j1_size = json1["Nervous system"]["size"]["value"]
+    j2_size = json2["Nervous system"]["size"]["value"]
+    return j1_size + j2_size
+
+
+def incNSvals(j1):
+    pass
+
+def addConnectedCell(j1, name):
+
+
