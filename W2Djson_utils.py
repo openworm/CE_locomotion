1b619d35cfa01768404eb0d54543d517b55e3448
adampdp <adampdp100@gmail.com>
2026-01-11 12:57:37 +0000

    json utility updates

--- W2Djson_utils.py
+++ W2Djson_utils.py
@@ -24,13 +24,37 @@ def addNewNeuron(j1, parameters):
     j1[NSname]["size"]["value"] = j1[NSname]["size"]["value"] + 1
     indVal = j1[NSname]["size"]["value"]
     for parval in jsonNames["List"][NSname]:
-        if parameters[NSname][parval] is dict:
-            j1[NSname][parval]["value"].append(parameters[NSname][parval]["value"])
+        if "value" not in j1[NSname][parval]:
+            j1[NSname][parval]["value"] = []
+        j1[NSname][parval]["value"].append(parameters[parval]["value"])
+        if "evolvable" in parameters[parval]:
+            if "evolvable" not in j1[NSname][parval]:
+                j1[NSname][parval]["evolvable"] = []
             j1[NSname][parval]["evolvable"].append(
-                {"ind":indVal, "val": parameters[NSname][parval]["evolvable"]})
-        else:
-            j1[NSname][parval]["value"].append(parameters[NSname][parval])
+                {"ind":indVal, "val": parameters[parval]["evolvable"]})
     return indVal
+
+def addConnection(j1, wp):
+    if wp["module"] not in jsonNames["Weights"]:
+        print(wp["module"], " not in ", jsonNames["Weights"])
+        exit()
+    if wp["type"] not in jsonNames["Weights"][wp["module"]]:
+        print(wp["type"], " not in ", jsonNames["Weights"][wp["module"]])
+    if wp["type"] not in j1[wp["module"]]:
+        j1[wp["module"]][wp["type"]] = {}
+        j1[wp["module"]][wp["type"]]["value"] = []
+    j1[wp["module"]][wp["type"]]["value"].append(wp["value"])    
+    if "evolvable" in wp:
+        if "evolvable" not in j1[wp["module"]][wp["type"]]:
+            j1[wp["module"]][wp["type"]]["evolvable"] = []
+        j1[wp["module"]][wp["type"]]["evolvable"].append(
+            {"from" : wp["value"]["from"], 
+             "to" : wp["value"]["to"],
+            "val" : wp["evolvable"]
+             }
+            ) 
+    
+
 
 def run(a=None, **kwargs):
     a = hf.build_namespace(hf.DEFAULTS, a, **kwargs)
@@ -41,14 +65,27 @@ def run(a=None, **kwargs):
     
     hf.make_directory("test_json_utils", overwrite=True)
     addedNeurons = []
-
-    parameters = {NSname : {"biases" : {"value" : -100, "evolvable" : 3},
-                            "taus" : -100, 
-                            "gains" : {"value" : -100, "evolvable" : 7}, 
-                            "states" : -100}}
-    addedNeurons.append({"index" : addNewNeuron(network_json_data, parameters), 
-                         "parameters" : parameters})
     
+
+    cell_parameters = {"biases" : {"value" : -100, "evolvable" : 3},
+                            "taus" : {"value": -100}, 
+                            "gains" : {"value" : -100, "evolvable" : 7}, 
+                            "states" : {"value":-100}
+                            }
+    addedNeurons.append({"index" : addNewNeuron(network_json_data, cell_parameters), 
+                         "parameters" : cell_parameters})
+    
+    weight_parameters = {"module": NSname,
+                         "type": "Chemical weights",
+                         "value" :
+                        {"from" : addedNeurons[-1]["index"], 
+                        "to" : 15, 
+                        "weight" : -1}, 
+                        "evolvable" : 2}                                   
+    addConnection(network_json_data, weight_parameters)
+    
+
+
     #print(addedNeurons)                
     #unity indices
 
