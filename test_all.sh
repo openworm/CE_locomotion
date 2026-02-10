a5c569012d3d77659cf14f026f31dd125027bb2a
adampdp <adampdp100@gmail.com>
2026-02-10 12:02:58 +0000

    modified test order

--- test_all.sh
+++ test_all.sh
@@ -85,6 +85,15 @@ if [ "$quick_test" == 0 ]; then
         python testW2D18.py
     fi;
 
+    omv test -V .test.izq_sim.omt
+
+    omv test -V .test.CO.omt
+    omv test -V .test.example.omt
+    omv test -V .test.CEW2D.omt
+    omv test -V .test.2021.omt
+    omv test -V .test.2021W2D.omt
+    omv test -V .test.W2DCE.omt
+
     omv test -V .test.osc_sim.omt
     omv test -V .test.osc_sim_21.omt
     omv test -V .test.osc_sim_21all.omt
@@ -93,13 +102,9 @@ if [ "$quick_test" == 0 ]; then
     omv test -V .test.W2DSR18E.omt
     omv test -V .test.COW2D.omt
     omv test -V .test.W2DCO.omt
-    omv test -V .test.izq_sim.omt
-    omv test -V .test.CO.omt
-    omv test -V .test.example.omt
-    omv test -V .test.CEW2D.omt
-    omv test -V .test.2021.omt
-    omv test -V .test.2021W2D.omt
-    omv test -V .test.W2DCE.omt
+    
+
+
     omv test -V .test.W2DSR.omt
     omv test -V .test.W2DSR21.omt
     omv test -V .test.W2DSRE21.omt
