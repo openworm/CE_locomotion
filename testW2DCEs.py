36d210bace25e84909345776628fee1c2338bca0
adampdp <adampdp100@gmail.com>
2026-03-12 17:01:06 +0000

    added W2DCEs test

---
+++ testW2DCEs.py
@@ -0,0 +1,24 @@
+from run_main import run
+
+run(
+    simduration=20,
+    simtransient=0,
+    popSize=96,
+    RandSeed=1233,
+    maxGens=5,
+    modelName="W2DCE",
+    modelFolder="Worm2D",
+    outputFolderName="testruns/exW2DCEs",
+    doEvol=True,
+    overwrite=True,
+    checkPointInterval=5,
+    reRand=True,
+    doPlotEvol=True,
+    doNML=False,
+    # doCPT=True,
+    evoType="EvoCE",
+    AvgSpeed=0.0001,
+    # SRType = "SR_TRANS_STRETCH"
+    doTestRun=True,
+    doOrigMuscInput=True,
+)
