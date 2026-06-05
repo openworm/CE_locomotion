490be3616e3d6d198c177fe0d630b835714cf297
Padraig Gleeson <p.gleeson@gmail.com>
2026-06-05 20:01:35 +0100

    add  ${OMV_ARGS}

--- test_all.sh
+++ test_all.sh
@@ -103,7 +103,7 @@ if [ "$quick_test" == 0 ]; then
     #omv test -V .test.CEW2D.omt #main.cpp test.example.mep
     omv test -V .test.W2DCEa.omt #main_osc.cpp test.example.mep
     omv test -V .test.W2DCEanm.omt #main_osc.cpp test.W2DCEanm.mep, as W2DCEa but origMusc=False
-    omv test -V .test.W2DCEanm2.omt
+    omv test -V .test.W2DCEanm2.omt ${OMV_ARGS}
     omv test -V .test.W2DCE.omt
     omv test -V .test.W2DCEs.omt
     omv test -V .test.W2DSR.omt
