92536affea75f8072f5c516e14883137cc3910aa
Padraig Gleeson <p.gleeson@gmail.com>
2026-07-23 16:51:54 +0100

    Disable one more test

--- test_all.sh
+++ test_all.sh
@@ -116,7 +116,7 @@ if [ "$quick_test" == 0 ]; then
    
 
 
-    omv test -V .test.COW2DSR2.omt
+    omv test -V .test.COW2DSR2.omt  ${OMV_ARGS}
     omv test -V .test.W2D18L.omt
     omv test -V .test.W2DSRFRswitch.omt
 
